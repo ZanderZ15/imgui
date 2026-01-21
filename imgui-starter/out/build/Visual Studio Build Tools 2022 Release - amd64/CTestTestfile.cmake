f471f074183895c1dbcd9b4facf6a3882717f5cc
ZanderZ15 <zandermillerbrown@gmail.com>
2026-01-20 16:56:26 -0800

    Working innit

---
+++ imgui-starter/out/build/Visual Studio Build Tools 2022 Release - amd64/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/zande/Advanced Programming_CMPM123/imgui/imgui-starter
+# Build directory: C:/Users/zande/Advanced Programming_CMPM123/imgui/imgui-starter/out/build/Visual Studio Build Tools 2022 Release - amd64
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
